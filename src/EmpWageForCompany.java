3232c59b0b67739a2db0e2cad813036756caff6b
Ngowda94 <Nagraj94>
2022-06-29 22:39:28 +0530

    added for employee part time

--- src/EmpWageForCompany.java
+++ src/EmpWageForCompany.java
@@ -1,16 +1,20 @@
 public class EmpWageForCompany {
     static int EMP_IS_FULL_DAY = 1;
     static int WAGES_PER_HRS = 20;
+    static int EMP_IS_HALF_DAY = 2;
     public static void main(String[] args) {
         int empHrs = 0;
         int dailyWages = 0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
         if (EMP_IS_FULL_DAY == empCheck)
         {
             System.out.println("employee is present for full day");
             empHrs = 8;
-        }
-        else
+        } else if (EMP_IS_HALF_DAY == empCheck)
+        {
+            System.out.println("employee is present for half day");
+            empHrs = 4;
+        } else
         {
             System.out.println("employee is absent");
             empHrs = 0;
