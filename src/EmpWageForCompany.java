4d88feaa67fb6312efd52f8c35466f45192714ff
Ngowda94 <Nagraj94>
2022-06-29 22:29:26 +0530

    welcome message

--- src/EmpWageForCompany.java
+++ src/EmpWageForCompany.java
@@ -1,14 +1,6 @@
 public class EmpWageForCompany {
-    static int EMP_PRESENT = 1;
+
     public static void main(String[] args) {
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        if (EMP_PRESENT == empCheck)
-        {
-            System.out.println("employee is present");
-        }
-        else
-        {
-            System.out.println("employee is absent");
-        }
+        System.out.println("Welcome to Employee Wage Computation");
     }
 }
